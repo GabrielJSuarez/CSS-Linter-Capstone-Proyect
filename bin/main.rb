edbaf5078cd07eba073028c46fe3834cf7e19161
Gabriel Suárez <ginnlaiho@gmail.com>
2020-10-16 19:06:17 -0500

    Add several changes: - Format linter_suite file - create css_caller file to get the file that's going to be checked - create class to get the file and call the method suite for the linter - create methods to run the linter and create the report of errors in the console

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,30 @@
 # frozen_string_literal: true
+
 # Caller
+
+require_relative '../lib/css_caller'
+
+def run_linter
+  error_message = []
+  call_file = Invoker.new
+  file = call_file.file_data
+  call_file.test_caller(file, error_message)
+  error_message
+end
+
+def create_report()
+  errors = 0
+  report = run_linter()
+  if report.length > 0
+    report.each do |x|
+      errors += 1
+      puts "#{errors} - #{x}" 
+    end
+  else
+    puts "No error found in file"
+  end
+end
+
+create_report()
+
+
