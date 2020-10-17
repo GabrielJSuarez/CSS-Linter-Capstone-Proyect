fe4cf4cc35d66e4feae3390bd14197eae7c76a6d
Gabriel Suárez <ginnlaiho@gmail.com>
2020-10-16 19:54:49 -0500

    security stage due to suspicious bevahiour of IDE

--- lib/linter_suite.rb
+++ lib/linter_suite.rb
@@ -1,7 +1,28 @@
 # frozen_string_literal: true
 
 # Linter module with all the methods to perform checks on a css file.
+
 module Linter
+  def trailing_white_space?(file, error_log)
+    errorlog = 'trailing white space on line'
+
+    file.each_with_index do |x, y|
+      error_log << "#{errorlog} #{y + 1}" if x.end_with?(" ")
+    end
+    error_log
+  end
+
+  def block_declaration_single_line?(file, error_log)
+    errorlog0 = 'More than one declaration in a single line, on line'
+
+    file.each_with_index do |x, y|
+      next if x.include?('}') || x == '' || x.start_with?("/*") || x.end_with?(";") || x.end_with?(" ")
+
+      error_log << "#{errorlog0} #{y + 1}" unless x.scan(/(,|{)/).length == 1
+    end
+    error_log
+  end
+
   def block_declaration_start?(file, error_log)
     errorlog1 = 'No space before opening block declaration on line'
 
@@ -17,7 +38,7 @@ module Linter
     errorlog2 = 'Expected indentation of two spaces in declaration on line'
 
     file.each_with_index do |x, y|
-      next if x.include?('{') || x.include?('}') || x == ''
+      next if x.include?('{') || x.include?('}') || x == '' || x.start_with?("/*") || x.end_with?(",") || x.end_with?(" ")
 
       error_log << "#{errorlog2} #{y + 1}" unless x.start_with?('  ')
     end
@@ -39,7 +60,7 @@ module Linter
     errorlog4 = 'Empty space expected after colon (:) in declaration on line'
 
     file.each_with_index do |x, y|
-      next if x.include?('{') || x.include?('}') || x == ''
+      next if x.include?('{') || x.include?('}') || x == '' || x.start_with?("/*") || x.end_with?(",") || x.end_with?(" ")
 
       error_log << "#{errorlog4} #{y + 1}" unless x.include?(': ')
     end
@@ -50,7 +71,7 @@ module Linter
     errorlog5 = 'Declaration should be in a single line, on line'
 
     file.each_with_index do |x, y|
-      next if x.include?('{') || x.include?('}') || x == ''
+      next if x.include?('{') || x.include?('}') || x == '' || x.start_with?("/*") || x.end_with?(",") 
 
       error_log << "#{errorlog5} #{y + 1}" unless x.scan(/;/).length < 2
     end
@@ -61,7 +82,7 @@ module Linter
     errorlog6 = 'Declaration should end with a semi-colon (Trailing white space?), on line'
 
     file.each_with_index do |x, y|
-      next if x.include?('{') || x.include?('}') || x == ''
+      next if x.include?('{') || x.include?('}') || x == '' || x.start_with?("/*") || x.end_with?(",")
 
       error_log << "#{errorlog6} #{y + 1}" unless x.end_with?(';')
     end
@@ -72,7 +93,7 @@ module Linter
     errorlog7 = 'Expected space after comma separated values in declaration, on line'
 
     file.each_with_index do |x, y|
-      next if x.include?('{') || x.include?('}') || x == ''
+      next if x.include?('{') || x.include?('}') || x == '' || x.start_with?("/*") || x.end_with?(",")
 
       error_log << "#{errorlog7} #{y + 1}" unless x.scan(/, */).all? { |z| z.length == 2 }
     end
@@ -83,7 +104,7 @@ module Linter
     errorlog8 = 'Unexpected prefix property float value on line'
 
     file.each_with_index do |x, y|
-      next if x.include?('{') || x.include?('}') || x == ''
+      next if x.include?('{') || x.include?('}') || x == '' || x.start_with?("/*") || x.end_with?(",")
 
       error_log << "#{errorlog8} #{y + 1}" if x.include?('0.')
     end
