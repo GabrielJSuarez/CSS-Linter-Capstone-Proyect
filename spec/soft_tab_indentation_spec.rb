505103ba3f1403769348d618de058c62fb602342
Gabriel Suárez <ginnlaiho@gmail.com>
2020-10-17 12:12:07 -0500

    create new file to divide test suite

---
+++ spec/soft_tab_indentation_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# spec/linter_spec.rb
+require '../lib/linter_suite.rb'
+include Linter
+
+describe Linter do
+    describe '#soft_tab_indentation?' do
+        let(:test_case_error) { ['padding: 0 .5rem;'] }
+        let(:test_case_no_error) { ['  padding: 0 .5rem;'] }
+    
+        context '#soft_tab_indentation? behaviour' do
+          example 'return error string when the correct indentation is not found' do
+            expect(soft_tab_indentation?(test_case_error, [])).to eq(['Expected indentation of two spaces in declaration on line 1'])
+          end
+    
+          example 'return empty array when no error is found' do
+            expect(soft_tab_indentation?(test_case_no_error, [])).to eq([])
+          end
+        end
+      end
+end
