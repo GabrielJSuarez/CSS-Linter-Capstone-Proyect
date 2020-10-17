f6432a544d193213cdbfb5493735870a9ec2bdf6
Gabriel Suárez <ginnlaiho@gmail.com>
2020-10-17 12:17:11 -0500

    create new file to divide test suite

--- spec/linter_spec.rb
+++ spec/linter_spec.rb
@@ -5,21 +5,6 @@ require '../lib/linter_suite.rb'
 include Linter
 
 describe Linter do
-  describe '#empty_line_after_block?' do
-    let(:test_case_error) { ['}', '.class {'] }
-    let(:test_case_no_error) { ['}', ''] }
-
-    context '#empty_line_after_block? behaviour' do
-      example 'return error string when there is no empty line after a closing block declaration line' do
-        expect(empty_line_after_block?(test_case_error, [])).to eq(['Expecting empty line after block closure, on line 1'])
-      end
-
-      example 'return empty array when no error is found' do
-        expect(empty_line_after_block?(test_case_no_error, [])).to eq([])
-      end
-    end
-  end
-
   describe '#empty_line_at_end?' do
     let(:test_case_error) { ['}'] }
     let(:test_case_no_error) { ['}', ''] }
