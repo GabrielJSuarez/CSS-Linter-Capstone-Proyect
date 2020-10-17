a651ef4849dcad232b040a51783b79511b1cfec7
Gabriel Suárez <ginnlaiho@gmail.com>
2020-10-17 12:11:11 -0500

    create new file to divide test suite

---
+++ spec/block_declaration_start_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# spec/linter_spec.rb
+require '../lib/linter_suite.rb'
+include Linter
+
+describe Linter do
+  describe '#block_declaration_start?' do
+    let(:test_case_error) { ['.bg-orange{'] }
+    let(:test_case_no_error) { ['.bg-orange {'] }
+
+    context '#block_declaration_start? behaviour' do
+      example 'return error string when no space is detected before the block declaration {' do
+        expect(block_declaration_start?(test_case_error, [])).to eq(['No space before opening block declaration on line 1'])
+      end
+
+      example 'return empty array when no error is found' do
+        expect(block_declaration_start?(test_case_no_error, [])).to eq([])
+      end
+    end
+  end
+end
